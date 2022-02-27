63335488a5a578d844bd33a1400033d6aa47c161
Krzysztof Sawicki <krzysiek.saw@gmail.com>
2022-02-27 16:32:45 +0100

    Added themed buttons and lables

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from faulthandler import disable
-from tkinter import *
+import tkinter as tk
+import tkinter.ttk as ttk
 import pickle
 from tkinter import messagebox
 import valuecheck
@@ -14,16 +15,16 @@ currencies =['AED', 'BRL', 'CAD', 'CHF', 'CNY',
 'RUB', 'SAR', 'SEK', 'SGD', 'THB', 'TRY', 'TWD', 
 'USD', 'ZAR']
 
-class Window(Toplevel):
+class Window(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
         self.title('Toplevel Window')
 
-        self.l1 = Label(self, text = "Portfolio value:").grid(row = 0, column = 0, padx= 5, pady=5)
-        self.t1 = Text(self, height = 1, width = 20)
+        self.l1 = ttk.Label(self, text = "Portfolio value:").grid(row = 0, column = 0, padx= 5, pady=5)
+        self.t1 = tk.Text(self, height = 1, width = 20)
 
-        self.l2 = Label(self, text = "Growth since last close (%):").grid(row = 1, column = 0, padx= 5, pady=5)
-        self.t2 = Text(self, height = 1, width = 20)
+        self.l2 = ttk.Label(self, text = "Growth since last close (%):").grid(row = 1, column = 0, padx= 5, pady=5)
+        self.t2 = tk.Text(self, height = 1, width = 20)
         
 
         self.sum = 0
@@ -37,47 +38,47 @@ class Window(Toplevel):
 
         self.refresh_window(round(self.sum, 2))
 
-        self.clicked = StringVar()
+        self.clicked = tk.StringVar()
         self.clicked.set('USD')
-        OptionMenu(self, self.clicked, *currencies, command=self.change_currency).grid(row = 0, column = 2)
+        ttk.OptionMenu(self, self.clicked, *currencies, command=self.change_currency).grid(row = 0, column = 2)
 
-        self.t2.delete("1.0", END)
-        self.t2.insert(END,self.percent_growth)
+        self.t2.delete("1.0", tk.END)
+        self.t2.insert(tk.END,self.percent_growth)
         self.t2.grid(row = 1, column = 1)
 
     def refresh_window(self, sum):
-        self.t1.delete("1.0", END)
-        self.t1.insert(END,sum)
+        self.t1.delete("1.0", tk.END)
+        self.t1.insert(tk.END,sum)
         self.t1.grid(row = 0, column = 1)
         
     def change_currency(self, event):
 
         changed = float(self.sum / float(currency.currency_covnertion(self.clicked.get())))
 
-        self.t1.delete("1.0", END)
-        self.t1.insert(END,round(changed, 2))
+        self.t1.delete("1.0", tk.END)
+        self.t1.insert(tk.END,round(changed, 2))
         self.t1.grid(row = 0, column = 1)
 
 
-class App(Tk):
+class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Main Window')
 
-        self.l1 = Label(self, text = "SYMBOL", font=("Helvetica",12)).grid(row = 0, column = 0, padx= 5, pady=5)
-        self.l2 = Label(self, text = "AMOUNT", font=("Helvetica",12)).grid(row = 0, column = 2, padx= 5, pady=5)
+        self.l1 = ttk.Label(self, text = "SYMBOL", font=("Helvetica",12)).grid(row = 0, column = 0, padx= 5, pady=5)
+        self.l2 = ttk.Label(self, text = "AMOUNT", font=("Helvetica",12)).grid(row = 0, column = 2, padx= 5, pady=5)
         
-        self.b1 = Button(self, text = "ADD", command = self.add_to_list)
+        self.b1 = ttk.Button(self, text = "ADD", command = self.add_to_list)
         self.b1.grid(row = 2, column = 1, padx= 5, pady=5)
         self.b1.config(state='active')
-        self.b2 = Button(self, text = "IMPORT", command = self.import_from_file).grid(row = 3, column = 0, padx= 5, pady=5)
-        self.b3 = Button(self, text = "EXPORT", command = self.export_to_file).grid(row = 3, column = 2, padx= 5, pady=5)
-        self.b4 = Button(self, text = "NEXT", command = self.open_window).grid(row = 3, column = 1, padx= 5, pady=5)
+        self.b2 = ttk.Button(self, text = "IMPORT", command = self.import_from_file).grid(row = 3, column = 0, padx= 5, pady=5)
+        self.b3 = ttk.Button(self, text = "EXPORT", command = self.export_to_file).grid(row = 3, column = 2, padx= 5, pady=5)
+        self.b4 = ttk.Button(self, text = "NEXT", command = self.open_window).grid(row = 3, column = 1, padx= 5, pady=5)
 
-        self.e1_value = StringVar()
-        self.e1 = Entry(self, textvariable = self.e1_value).grid(row = 1, column = 0, padx= 5, pady=5)
-        self.e2_value = StringVar()
-        self.e2 = Entry(self, textvariable = self.e2_value).grid(row = 1, column = 2, padx= 5, pady=5)
+        self.e1_value = tk.StringVar()
+        self.e1 = ttk.Entry(self, textvariable = self.e1_value).grid(row = 1, column = 0, padx= 5, pady=5)
+        self.e2_value = tk.StringVar()
+        self.e2 = ttk.Entry(self, textvariable = self.e2_value).grid(row = 1, column = 2, padx= 5, pady=5)
 
 
     def open_window(self):
@@ -88,16 +89,15 @@ class App(Tk):
             messagebox.showwarning(title="Warning", message="Empty portfolio")
 
     def incorrect_value(self):
-        self.l3 = Label(self, text = "Enter correct value", font=("Helvetica",8), fg="red").grid(row = 2, column = 2, padx= 5, pady=5)
+        self.l3 = ttk.Label(self, text = "Enter correct value", font=("Helvetica",8), fg="red").grid(row = 2, column = 2, padx= 5, pady=5)
         self.e2_value.set('')
 
     def add_to_list(self):
 
-        if len(self.e2_value.get()) == 0:
-                self.incorrect_value()
-                return None
-
         try:
+            if len(self.e2_value.get()) == 0 or float(self.e2_value.get()) < 0:
+                    self.incorrect_value()
+                    return None
             symbol = str(self.e1_value.get())
             amount = float(self.e2_value.get())
         except ValueError:
@@ -117,7 +117,7 @@ class App(Tk):
             for i in dict_of_portfolio :
                 print_records += (str(i) + " : " + str(dict_of_portfolio[i])) + "\n"
 
-            self.l3 = Label(self, text = print_records, font="Helvetica,12,bold").grid(row=5, column=0)
+            self.l3 = ttk.Label(self, text = print_records, font=("Helvetica",12)).grid(row=5, column=0)
         elif response.status_code != 200:
             messagebox.showwarning(title="Warning", message="Symbol unknown")
 
@@ -130,7 +130,7 @@ class App(Tk):
         for i in dict_of_portfolio :
             print_records += (str(i) + " : " + str(dict_of_portfolio[i])) + "\n"
         
-        self.l3 = Label(self, text = print_records).grid(row=5, column=0)
+        self.l3 = ttk.Label(self, text = print_records).grid(row=5, column=0)
 
     def export_to_file(self):
         a_file = open("data.pkl", "wb")
